0b8c7f815eace21046e7027b28938cef75812379
Kevin Marrec <kevin@marrec.io>
2019-01-31 20:37:34 +0100

    fix: config hooks

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 export { default as build } from './build'
 export { default as head } from './head'
-export { default as hooks } from './head'
+export { default as hooks } from './hooks'
 export { default as manifest } from './manifest'
 export { default as meta } from './meta'
